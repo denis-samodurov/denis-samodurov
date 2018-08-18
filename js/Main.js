708e502aca58eb075ae18fe42acc2a32322cddca
denis-samodurov <samodurov.v.denis@gmail.com>
2018-08-18 17:44:29 +0300

    Дабивил функционал написать нам и оставить отзыв

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,26 @@
-$('#infoForAppleBeta').css("visibility", "hidden");
-$('input[name="appType"]').on("change", function () {
-    if($('[name="appType"]:checked').val() === 'apple'){
-        $('#infoForAppleBeta').css("visibility", "visible");
-    }else{
-        $('#infoForAppleBeta').css("visibility", "hidden");
-    }
+$(document).ready(function() {
+    $('#infoForAppleBeta').css("visibility", "hidden");
+    $('input[name="appType"]').on("change", function () {
+        if($('[name="appType"]:checked').val() === 'apple'){
+            $('#infoForAppleBeta').css("visibility", "visible");
+        }else{
+            $('#infoForAppleBeta').css("visibility", "hidden");
+        }
+    });
+
+    $(document).on('submit', '#betaForm', function() {
+        // do your things
+        return false;
+    });
+
+    var addMessage = firebase.functions().httpsCallable('emailReciever');
+    $(document).on("click", "#sendUserMail", function () {
+        const emailText = $('input[name="email"]').val();
+        console.log(emailText);
+        addMessage({email: emailText}).then(function(result) {
+            console.log("OK 200");
+        }).catch(function(error) {
+            console.log("ERROR");
+        });
+    });
 });
